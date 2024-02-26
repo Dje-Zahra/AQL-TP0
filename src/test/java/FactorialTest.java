2549dfa9744da849ca520fe2c6763ffbcaab827e
Zahra <zahra.25.djedaidi@gmail.com>
2024-02-26 03:00:19 -0800

    c

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -5,6 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class FactorialTest {
 
     @Test
-    void factorial() {
+    void noNeg() {
+        assertThrows(IllegalArgumentException.class,()->Factorial.factorial(-1));
+    }
+
+    @Test
+    void oneFact(){
+        assertEquals(1, Factorial.factorial(1));
+    }
+
+    @Test
+    void other(){
+        assertEquals(39916800, Factorial.factorial(11));
     }
 }
